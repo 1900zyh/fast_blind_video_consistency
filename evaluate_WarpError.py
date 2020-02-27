cef7f547a99f82ced2740de9a49e2e4d556492d1
1900zyh <zengyh7@mail2.sysu.edu.cn>
2020-02-27 21:46:47 +0800

    evaluate warp_error

--- evaluate_WarpError.py
+++ evaluate_WarpError.py
@@ -16,71 +16,63 @@ import networks
 import utils
 
 
+def read_frame_from_videos(vname):
+  frames = []
+  vidcap = cv2.VideoCapture(vname)
+  success, image = vidcap.read()
+  count = 0
+  while success:
+    frames.append(image)
+    success,image = vidcap.read()
+    count += 1
+  return frames
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Fast Blind Video Temporal Consistency')
     
 
     ### testing options
-    parser.add_argument('-task',            type=str,     required=True,            help='evaluated task')
-    parser.add_argument('-method',          type=str,     required=True,            help='test model name')
-    parser.add_argument('-dataset',         type=str,     required=True,            help='test datasets')
-    parser.add_argument('-phase',           type=str,     default="test",           choices=["train", "test"])
     parser.add_argument('-data_dir',        type=str,     default='data',           help='path to data folder')
-    parser.add_argument('-list_dir',        type=str,     default='lists',          help='path to list folder')
-    parser.add_argument('-redo',            action="store_true",                    help='redo evaluation')
+
+    parser.add_argument('-dataset',         type=str,     required=True,            help='test datasets')
+    parser.add_argument('-r', '--resume', default=None, type=str, required=True)
+    parser.add_argument('-n', '--name', default=None, type=str, required=True)
 
     opts = parser.parse_args()
-    opts.cuda = True
-
     print(opts)
 
-    output_dir = os.path.join(opts.data_dir, opts.phase, opts.method, opts.task, opts.dataset)
 
     ## print average if result already exists
-    metric_filename = os.path.join(output_dir, "WarpError.txt")
-    if os.path.exists(metric_filename) and not opts.redo:
-        print("Output %s exists, skip..." %metric_filename)
-
-        cmd = 'tail -n1 %s' %metric_filename
-        utils.run_cmd(cmd)
-        sys.exit()
-    
-
-    ## flow warping layer
-    device = torch.device("cuda" if opts.cuda else "cpu")
-    flow_warping = Resample2d().to(device)
+    metric_filename = os.path.join(opts.data_dir, opts.dataset, opts.name, "WarpError.txt")
+    flow_warping = Resample2d().cuda()
 
     ### load video list
-    list_filename = os.path.join(opts.list_dir, "%s_%s.txt" %(opts.dataset, opts.phase))
-    with open(list_filename) as f:
-        video_list = [line.rstrip() for line in f.readlines()]
+    print("===> Begin reading videos")
+    if opts.name == 'orig':
+        video_list = list(glob.glob(os.path.join(opts.resume, '*/orig.avi')))  
+    else:
+        video_list = list(glob.glob(os.path.join(opts.resume, '*/comp.avi')))
+
+    print("===> Begin processing {} videos".format(len(video_list)))
 
     ### start evaluation
     err_all = np.zeros(len(video_list))
 
-    for v in range(len(video_list)):
+    for v, video in enumerate(video_list):
+        video_name = video.split('/')[-2]
 
-        video = video_list[v]
-
-        frame_dir = os.path.join(opts.data_dir, opts.phase, opts.method, opts.task, opts.dataset, video)
-        occ_dir = os.path.join(opts.data_dir, opts.phase, "fw_occlusion", opts.dataset, video)
-        flow_dir = os.path.join(opts.data_dir, opts.phase, "fw_flow", opts.dataset, video)
-        
-        frame_list = glob.glob(os.path.join(frame_dir, "*.jpg"))
+        occ_dir = os.path.join(opts.data_dir, opts.dataset, opts.name, "fw_occlusion", video_name)
+        flow_dir =  os.path.join(opts.data_dir, opts.dataset, opts.name, "fw_flow", video_name)
+        frame_list = read_frame_from_videos(video)
 
         err = 0
         for t in range(1, len(frame_list)):
-            
-            
             ### load input images
-            filename = os.path.join(frame_dir, "%05d.jpg" %(t - 1)) 
-            img1 = utils.read_img(filename)
-            filename = os.path.join(frame_dir, "%05d.jpg" %(t))
-            img2 = utils.read_img(filename)
-
-            print("Evaluate Warping Error on %s-%s: video %d / %d, %s" %(opts.dataset, opts.phase, v + 1, len(video_list), filename))
-
+            img1 = frame_list[t-1]
+            img2 = frame_list[t]
+            print("Evaluate error for {}, {}/{} on frame {}/{}".format(video_name,
+                v, len(video_list), t, len(frame_list)))
 
             ### load flow
             filename = os.path.join(flow_dir, "%05d.flo" %(t-1))
@@ -92,30 +84,22 @@ if __name__ == "__main__":
             noc_mask = 1 - occ_mask
 
             with torch.no_grad():
-
                 ## convert to tensor
-                img2 = utils.img2tensor(img2).to(device)
-                flow = utils.img2tensor(flow).to(device)
-
+                img2 = utils.img2tensor(img2).cuda()
+                flow = utils.img2tensor(flow).cuda()
                 ## warp img2
                 warp_img2 = flow_warping(img2, flow)
-
                 ## convert to numpy array
                 warp_img2 = utils.tensor2img(warp_img2)
 
-
             ## compute warping error
             diff = np.multiply(warp_img2 - img1, noc_mask)
-            
             N = np.sum(noc_mask)
             if N == 0:
                 N = diff.shape[0] * diff.shape[1] * diff.shape[2]
-
             err += np.sum(np.square(diff)) / N
-            
         err_all[v] = err / (len(frame_list) - 1)
-
-
+        print(err / (len(frame_list) - 1))
     print("\nAverage Warping Error = %f\n" %(err_all.mean()))
 
     err_all = np.append(err_all, err_all.mean())
